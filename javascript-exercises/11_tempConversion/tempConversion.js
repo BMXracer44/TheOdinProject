0a876273f43ccb500bd089fb78ab467dbeb99b5b
Flex <fergusalex444@gmail.com>
2025-07-01 11:14:59 -0500

    Temp conversion exercise

--- javascript-exercises/11_tempConversion/tempConversion.js
+++ javascript-exercises/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let num = (temp - 32) * (5/9);
+  if(num % 1 == 0){
+    return num;
+  }
+  return num.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let num = (temp * (9/5)) + 32;
+  if(num % 1 == 0){
+    return num;
+  }
+  return num.toFixed(1);
 };
 
 // Do not edit below this line
