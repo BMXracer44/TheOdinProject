e7239b2bd4f95acd7accc809ed1ac43662f6c483
Flex <fergusalex444@gmail.com>
2025-07-01 13:52:23 -0500

    Fib sequence

--- javascript-exercises/14_fibonacci/fibonacci.js
+++ javascript-exercises/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0){
+        return "OOPS";
+    }
+    if(num == 1){
+        return 1;
+    }
+    let fib1 = 1;
+    let fib2 = 0;
+    let seq = 0;
+    for(let i = 2; i <= num; i++){
+        seq = fib1 + fib2;
+        fib2 = fib1;
+        fib1 = seq;
+    }
+    return seq;
 };
 
 // Do not edit below this line
