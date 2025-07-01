2b1abc83eb300ed6ba6bce45ea16fc23a34c9e41
Flex <fergusalex444@gmail.com>
2025-07-01 12:21:50 -0500

    Palindromes exercise

--- javascript-exercises/13_palindromes/palindromes.js
+++ javascript-exercises/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+    console.log(string);
+    if(cleanedString.split("").reverse().join("") == cleanedString){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
