66154a9f1446e13c8fefad47a4b759611a5c4395
Flex <fergusalex444@gmail.com>
2025-07-01 11:31:48 -0500

    Javascript calculator

--- javascript-exercises/12_calculator/calculator.js
+++ javascript-exercises/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(const num of arr){
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for(const num of arr){
+    result *= num;
+  };
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  while(num > 0){
+    fact *= num;
+    num--; 
+  }
+  return fact;
 };
 
 // Do not edit below this line
