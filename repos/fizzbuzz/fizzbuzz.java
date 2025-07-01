e6395ccb8f07af8d5900e6935a2862b166dc26be
Alex Ferguson <fergusalex444@gmail.com>
2025-07-01 16:11:58 -0500

    Added user input

--- repos/fizzbuzz/fizzbuzz.java
+++ repos/fizzbuzz/fizzbuzz.java
@@ -1,22 +1,24 @@
-import java.utils.*;
+import java.util.*;
 
 class fizzbuzz{
-    public void class main(String args[]){
+    public static void main(String args[]){
         Scanner input = new Scanner(System.in);
-        int answer = parseInt(prompt("Enter the number of iterations: "));
+        System.out.print("Enter the number of iterations: ");
+        String userInput = input.nextLine();
+        int answer = Integer.parseInt(userInput);
 
-        for(let i = 1; i <= answer; i++){
+        for(int i = 1; i <= answer; i++){
             if((i % 3 == 0) && (i % 5 == 0)){
                 System.out.println("FizzBuzz");
             }
-            else if(i % 3 === 0){
+            else if(i % 3 == 0){
                 System.out.println("Fizz");
             }
             else if(i % 5 == 0){
                 System.out.println("Buzz");
             }
             else{
-                System.out.println(i)
+                System.out.println(i);
             }
         }
     }
